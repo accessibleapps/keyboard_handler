685e295de567aa8b146bfd43d10a493b5cd1e2a7
Q <q@qwitter-client.net>
2010-12-19 19:50:29 -0500

    Transformed keyboard_handler into a package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+__version__ = 0.2
+
+setup(
+name = "Keyboard_Handler",
+ version = __version__,
+ author = "Christopher Toth",
+ packages = find_packages(),
+ )
