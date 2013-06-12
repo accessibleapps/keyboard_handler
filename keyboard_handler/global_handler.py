7dc958d221170e8cbb65771fc6c59e74a80dc72d
Q <q@q-continuum.net>
2013-06-12 03:50:50 -0600

    A stab at making the mac keyboard handler work

--- keyboard_handler/global_handler.py
+++ keyboard_handler/global_handler.py
@@ -1,5 +1,7 @@
 import platform
 if platform.system() == 'Linux':
  from linux import LinuxKeyboardHandler as GlobalKeyboardHandler
-else:
+elif platform.system() == 'Windows':
  from wx_handler import WXKeyboardHandler as GlobalKeyboardHandler
+elif platform.system() == 'Darwin':
+ from osx import OSXKeyboardHandler as GlobalKeyboardHandler
