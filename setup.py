f365ed3908a2a849135bea5bf55f814ea7a53d7a
Christopher Toth <q.alpha@gmail.com>
2019-08-11 16:22:45 -0600

    Setup.py cleanups

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup, find_packages
 __version__ = 0.52
 
 setup(
-name = "Keyboard_Handler",
- version = __version__,
- author = "Christopher Toth",
- packages = find_packages(),
- )
+    name="Keyboard_Handler",
+    version=__version__,
+    description="Hook global and local keystrokes on multiple platforms",
+    author="Christopher Toth",
+    packages=find_packages(),
+)
